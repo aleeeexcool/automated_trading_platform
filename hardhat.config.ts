cc01979542d24562efd56e85930096536661528b
aleeeexcool <ramzhev.sasha@outlook.com>
2023-11-17 17:30:48 +0200

    config upd

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,38 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.14",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+            details: {
+              yul: true,
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      deploy: ["./scripts/deploy/bsc/"],
+      allowUnlimitedContractSize: true,
+    },
+    localhost: {
+      deploy: ["./scripts/deploy/ethereum/"],
+    },
+  },
+  mocha: {
+    timeout: 400000,
+  },
 };
 
 export default config;
