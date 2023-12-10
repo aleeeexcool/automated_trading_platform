f86087153699084d8c66f1e166c4d25d1299a487
aleeeexcool <aleeeexcool@gmail.com>
2023-12-10 20:41:25 +0200

    constants add accumulator address, max_amount, uint128

--- src/core/common/constants.ts
+++ src/core/common/constants.ts
@@ -3,3 +3,11 @@ import { ChainId } from "./chainId";
 export const NETWORK_RPC_URLS: { [key in ChainId]?: string[] } = {
   [ChainId.MAINNET]: [`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`],
 };
+
+export const ACCUMULATOR_ADDRESSES: { [key in ChainId]?: string } = {
+  [ChainId.MAINNET]: "0x0", // TODO populate after deploy
+};
+
+export const MAX_AMOUNT = "MAX";
+
+export const MAX_UINT128 = "340282366920938463463374607431768211455";
