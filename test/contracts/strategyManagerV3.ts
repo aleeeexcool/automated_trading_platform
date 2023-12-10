2be504f5ccc994930f31a74478abf3404abb17ef
aleeeexcool <aleeeexcool@gmail.com>
2023-12-10 20:48:58 +0200

    strategyManagerV3 test

---
+++ test/contracts/strategyManagerV3.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { config, deployments, ethers } from "hardhat";
+import { Signer } from "ethers";
+import {
+  GenericERC20,
+  StrategyManagerV3,
+} from "../../typechain";
+
+describe("StrategyManagerV3", function () {
+  
+});
+
